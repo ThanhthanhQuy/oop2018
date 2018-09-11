e79dcc69f297390d7f9c9ceaed427e8f8b6b2a6e
ThanhthanhQuy <38715034+ThanhthanhQuy@users.noreply.github.com>
2018-09-12 00:35:13 +0700

    update2

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -1,17 +1,36 @@
 package week1;
 
-public class Student {
+class Student {
 
     // TODO: khai báo các thuộc tính cho Student
 
+    private String name;
+    private String group;
+    private String id;
+    private String email;
+
     // TODO: khai báo các phương thức getter, setter cho Student
+    public String getName() { return name;}
+    public void setName(String _name) { name =_name ;}
+
+    public String getGroup() { return group;}
+    public void setGroup(String _group) { group = _group ;}
+
+    public String getId() { return id;}
+    public void setId(String _Id) { id = _Id ;}
+
+    public String getEmail() { return email;}
+    public void setEmail(String _email) { email = _email ;}
 
     /**
      * Constructor 1
      */
     Student() {
-        // TODO:
-    }
+        name="Student";
+        group="INT22041";
+        id="000";
+        email="uet@vnu.edu.vn";
+    } //
 
     /**
      * Constructor 2
@@ -21,6 +40,10 @@ public class Student {
      */
     Student(String n, String sid, String em) {
         // TODO:
+        name=n;
+        id=sid;
+        email=em;
+        group="INT22041";
     }
 
     /**
@@ -28,10 +51,15 @@ public class Student {
      * @param s
      */
     Student(Student s) {
+        name= s.name;
+        id=s.id;
+        group=s.group;
+        email=s.email;
         // TODO:
     }
 
     String getInfo() {
+        System.out.println("Sinh vien "+ name+ " hoc lop "+ group+" co id va email "+ id+","+ email);
         // TODO:
         return null; // xóa dòng này sau khi cài đặt
     }
--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -1,24 +1,101 @@
 package week1;
 
+
 public class StudentManagement {
 
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
+    private Student[] groupstudent= new Student[100];
+    int count=0;
 
-    public boolean sameGroup(Student s1, Student s2) {
+
+
+    public static boolean sameGroup(Student s1, Student s2) {
         // TODO:
-        return false; // xóa dòng này sau khi cài đặt
+        String gr1= s1.getGroup();
+        String gr2= s2.getGroup();
+       return gr1.equals(gr2);
+        // xóa dòng này sau khi cài đặt
     }
+     public void list(Student stu) { groupstudent[count]=stu; count++; }
 
+    public void Showlist() {
+        for(int i=0;i<count;i++) {
+            System.out.println(groupstudent[i].getName()+ " ");
+        }
+
+    }
     void studentsByGroup() {
         // TODO:
+        boolean A[] = new boolean[count];
+        for (int i = 0; i < count; i++) {
+            A[i] = true;
+        }
+        for (int i = 0; i < count; i++) {
+            if (A[i] == true) {
+                String t = groupstudent[i].getGroup();
+                System.out.print("Sinh vien thuoc lop " + t + " bao gom: ");
+                for (int j = 0; j < count; j++) {
+                    if (groupstudent[j].getGroup().equals(t)) {
+                        System.out.print(groupstudent[j].getName()+ " ");
+                        A[j] = false;
+                    }
+                }
+            }
+            System.out.print("\n");
+        }
     }
 
-    void removeStudent(String id) {
-        // TODO:
+
+    void removeStudent(String id){
+        for (int i = 0; i < count; i++) {
+            if (groupstudent[i].getId() == id) {
+                for (int j = i; j < count-1 ; j++) {
+                    groupstudent[j] = groupstudent[j + 1];
+                }
+                count--;
+            }
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello");
         // TODO:
+        StudentManagement list1= new StudentManagement();
+        Student studen1= new Student();
+        studen1.setName("Thanh Quy");
+        studen1.setEmail("thanhquysakura2911@gmail.com");
+        studen1.setGroup("INT22042");
+        studen1.setId("17020991");
+        String myname= studen1.getName();
+        System.out.println(myname);
+        studen1.getInfo();
+
+        Student studen11= new Student();
+        studen11.setName("Thanh ly");
+        studen11.setEmail("thanhquysakura2911@gmail.com");
+        studen11.setGroup("INT22044");
+        studen11.setId("17020991");
+        String myname1= studen11.getName();
+
+
+
+        Student student2= new Student("Huyen Thanh","17020889","anhTho@gmail.com");
+        Student student3= new Student("Nguyen Duong","17020879","batbat@gmail.com");
+        Student student4 = new Student(studen1);
+        student2.getInfo();
+        student3.getInfo();
+        student4.getInfo();
+        System.out.println(sameGroup(student2, student3));
+        System.out.println(sameGroup(student2, student4));
+
+       list1.list(student2); list1.list(student3); list1.list(student4); list1.list(studen11);
+        list1. Showlist();
+       list1.removeStudent("17020889");
+       list1. Showlist();
+
+       list1.studentsByGroup();
+
+
+
+
     }
 }
