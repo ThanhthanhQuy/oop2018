d0035064b1e4541afac15f3875db6afc7bc98606
ThanhthanhQuy <38715034+ThanhthanhQuy@users.noreply.github.com>
2018-09-12 15:51:30 +0700

    update3

--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -4,23 +4,29 @@ package week1;
 public class StudentManagement {
 
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
-    private Student[] groupstudent= new Student[100];
+    private Student[] students= new Student[100];
     int count=0;
 
 
 
-    public static boolean sameGroup(Student s1, Student s2) {
+    public boolean sameGroup(Student s1, Student s2) {
         // TODO:
         String gr1= s1.getGroup();
         String gr2= s2.getGroup();
        return gr1.equals(gr2);
         // xóa dòng này sau khi cài đặt
     }
-     public void list(Student stu) { groupstudent[count]=stu; count++; }
+     public void list(Student stu) {
+        if(count>100) System.out.println("Vuot qua so luong sinh vien cho phep");
+        else {
+            students[count] = stu;
+            count++;
+        }
+    }
 
     public void Showlist() {
         for(int i=0;i<count;i++) {
-            System.out.println(groupstudent[i].getName()+ " ");
+            System.out.println(students[i].getName()+ " ");
         }
 
     }
@@ -32,11 +38,11 @@ public class StudentManagement {
         }
         for (int i = 0; i < count; i++) {
             if (A[i] == true) {
-                String t = groupstudent[i].getGroup();
+                String t = students[i].getGroup();
                 System.out.print("Sinh vien thuoc lop " + t + " bao gom: ");
                 for (int j = 0; j < count; j++) {
-                    if (groupstudent[j].getGroup().equals(t)) {
-                        System.out.print(groupstudent[j].getName()+ " ");
+                    if (students[j].getGroup().equals(t)) {
+                        System.out.print(students[j].getName()+ " ");
                         A[j] = false;
                     }
                 }
@@ -48,9 +54,9 @@ public class StudentManagement {
 
     void removeStudent(String id){
         for (int i = 0; i < count; i++) {
-            if (groupstudent[i].getId() == id) {
+            if (students[i].getId() == id) {
                 for (int j = i; j < count-1 ; j++) {
-                    groupstudent[j] = groupstudent[j + 1];
+                    students[j] = students[j + 1];
                 }
                 count--;
             }
@@ -80,16 +86,19 @@ public class StudentManagement {
 
         Student student2= new Student("Huyen Thanh","17020889","anhTho@gmail.com");
         Student student3= new Student("Nguyen Duong","17020879","batbat@gmail.com");
-        Student student4 = new Student(studen1);
+         Student student4 = new Student(studen1);
         student2.getInfo();
         student3.getInfo();
         student4.getInfo();
-        System.out.println(sameGroup(student2, student3));
-        System.out.println(sameGroup(student2, student4));
+        System.out.println(list1.sameGroup(student2, student3));
+        System.out.println(list1.sameGroup(student2, student4));
 
        list1.list(student2); list1.list(student3); list1.list(student4); list1.list(studen11);
+       System.out.println("Danh sach sv trc khi xoa: ");
         list1. Showlist();
-       list1.removeStudent("17020889");
+
+       list1.removeStudent("17020879");
+        System.out.println("Danh sach sv sau khi xoa: ");
        list1. Showlist();
 
        list1.studentsByGroup();
