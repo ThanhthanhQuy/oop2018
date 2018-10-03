ca174a87dcfda2c4dd9e3f4e7e8132214180e860
ThanhthanhQuy <38715034+ThanhthanhQuy@users.noreply.github.com>
2018-10-03 14:50:53 +0700

    update3

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -4,22 +4,52 @@ public class Week3 {
 
     public static int max(int m, int n) {
         // TODO: Tìm giá trị lớn nhất của hai số nguyên, giá trị trả về của hàm là số lớn nhất
-        return -1;
+        double m1= (double)m;
+        double result=(((m1+n)/2)+ Math.abs((m1-n)/2));
+        return (int)result ;
     }
 
     public static int minOfArray(int[] array) {
         // TODO: Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
-        return -1;
+        if (array.length > 100 || array.length<=0 ) {
+            return 0;
+        }
+        else {
+            int min = array[0];
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] < min) {
+                    min = array[i];
+                }
+            }
+            return min;
+        }
     }
 
-    /**
-     * Chương trình tính chỉ số BMI và in ra kết quả đánh giá
-     * @param weight cân nặng
-     * @param height chiều cao
-     * @return Thiếu cân, Bình thường, Thừa cân, Béo phì
-     */
-    public static String calculateBMI(double weight, double height) {
-        // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
-        return null;
+        /**
+         * Chương trình tính chỉ số BMI và in ra kết quả đánh giá
+         * @param weight cân nặng
+         * @param height chiều cao
+         * @return Thiếu cân, Bình thường, Thừa cân, Béo phì
+         */
+        public static String calculateBMI ( double weight, double height){
+            // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
+            if (height <= 0 || weight < 0) {
+                return null;
+            } else {
+                double BMI = weight / (height*height);
+              //  System.out.println(BMI);
+                String BMI1="";
+                if (BMI < 18.5) BMI1 = "Thiếu cân";
+                else if (BMI >= 18.5 && BMI < 23) BMI1 = "Bình thường";
+                else if (BMI >= 23 && BMI <= 24.99) BMI1 = "Thừa cân";
+                else if (BMI > 25) BMI1 = "Béo phì";
+                return BMI1;
+            }
+        }
+        public static void main(String[] argvs) {
+            System.out.println(Week3.max(-4,-5));
+            System.out.println(calculateBMI(49, 1.54));
+            int[]arr={1,3,2,5,4};
+            System.out.println(minOfArray(arr));
+        }
     }
-}
--- test/week3/Week3Test.java
+++ test/week3/Week3Test.java
@@ -1,10 +1,43 @@
 package week3;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class Week3Test {
-
+    //mvn -DskipTests compile
     // TODO: Viết 5 testcase cho phương thức max()
+    @Test
+    public void TestMax() {
+        assertEquals(4, Week3.max(3,4) );
+        assertEquals( 0, Week3.max(0,0));
+        assertEquals(-5,Week3.max(-5,-5));
+        assertEquals(3334, Week3.max(3334, 5));
+        assertEquals(4, Week3.max(-4,4));
+    }
 
     // TODO: Viết 5 testcase cho phương thức minOfArray()
+    @Test
+        public void TestminOfArray() {
+        int[] arr= {1,2,3,4};
+        assertEquals(1, Week3.minOfArray(arr));
+        int[] arr1={-4};
+        assertEquals(-4, Week3.minOfArray(arr1));
+        int[] arr2={-1,-1, -1,-1};
+        assertEquals(-1,Week3.minOfArray(arr2));
+        int[] arr3={5,6,3,-5,6,0};
+        assertEquals(-5,Week3.minOfArray(arr3));
+        int[] arr4={57,315,227,1333,44,89,123,44,56};
+        assertEquals(44,Week3.minOfArray(arr4));
 
+    }
     // TODO: Viết 5 testcase cho phương thức calculateBMI()
+    @Test
+    public void TestcalculateBMI() {
+        assertEquals("Thừa cân", Week3.calculateBMI(51.75,1.5));
+        assertEquals("Thiếu cân", Week3.calculateBMI(33,1.54));
+        assertEquals("Bình thường", Week3.calculateBMI(49,1.54));
+        assertEquals("Béo phì", Week3.calculateBMI(90,1.54));
+        assertEquals(null, Week3.calculateBMI(-70,0));
+    }
+
+
 }
